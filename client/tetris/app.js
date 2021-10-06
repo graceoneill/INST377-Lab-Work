ab1f892593fa09a80dd30eaea50557d0330172d6
graceoneill <goneill1@terpmail.umd.edu>
2021-10-06 14:55:57 -0400

    arrays created

--- client/tetris/app.js
+++ client/tetris/app.js
@@ -1,5 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll(.grid div))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
+    const width = 10
+
+    //The Tetromnioes
+
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, ]
+    ]
+
+    const zTetromino = 'secondShape'
+
+    const oTetromino = 'thirdShape'
+
+    const iTetromino = 'fourthShape'
+
+    const tTetromino = 'fifthshape'
+
+    const tetrominos = [lTetromino, zTetromino, oTetromino, iTetromino, tTetromino]
 
     
+
 
 })
