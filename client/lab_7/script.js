299e41c7206b301f10462866dbe89b091a245437
graceoneill <goneill1@terpmail.umd.edu>
2021-10-17 23:48:50 -0400

    all done

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,7 +1,9 @@
 
-const mymap = L.map('mapid').setView([38.83986, -76.941642], 12);
+const mymap = L.map('mapid').setView([38.83986, -76.941642], 11);
+
 
 function mapInit() {
+
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -11,7 +13,6 @@ function mapInit() {
         zoomOffset: -1,
         accessToken: 'pk.eyJ1IjoiZ3JhY2VvbmVpbGwiLCJhIjoiY2t1cTduM2Z1NHNtdjJvbW4wbHEydGx5biJ9.Hpd_XkD5gXm4Msqjq9Vv6Q'
     }).addTo(mymap);
-
 
 }
 
@@ -39,32 +40,23 @@ async function dataHandler() {
 
         let markers = []
 
-        markers.forEach( marker => {
-            marker.remove();
-            });
-        
         const matchArray = findMatches(event.target.value, restaurant);
         let slicedMatch = matchArray.slice(0, 5);
 
         const html = slicedMatch.map(place => {
             const regex = new RegExp(event.target.value, 'gi')
             const nameName = place.name 
-            const categoryName = place.category
             const addressName = place.address_line_1
-            const cityName = place.city
-            const zipName = place.zip
-
-
-
+ 
             if (place.hasOwnProperty('geocoded_column_1')) {
                 const point = place.geocoded_column_1
                 const latlong = point.coordinates
                 const marker = latlong.reverse()
+
                 markers.push(L.marker(marker).addTo(mymap))
-                console.log(point)
-                
+       
             }
-                
+
             return `
                 <li>
                     <div class="box">
@@ -79,6 +71,8 @@ async function dataHandler() {
     
         suggestions.innerHTML = html;
 
+        
+
     }
 
     const searchInput = document.querySelector('.search');
@@ -86,7 +80,6 @@ async function dataHandler() {
     
     searchInput.addEventListener('change', displayMatches);
     searchInput.addEventListener('keyup', (evt) => { displayMatches(evt); });    
-
 
 }
 
